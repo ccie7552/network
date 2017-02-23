c9afbdb6db84e656c6d394cc320909726362435a
jaehakim <ccie7552@gmail.com>
2017-02-23 14:55:28 +0900

    Done

--- myTest1.py
+++ myTest1.py
@@ -1,15 +1,38 @@
-def Mod2(i):
-	if i%2 :
-		print "It's ODD"
-	else:
-		print "It's EVEN"
+"
+my_int = int(7)
+my_float = float(1.28)
+my_bool = bool(True)
 
-something=raw_input("Type I Valuse : ")
-i=something
-j=int(i)+1
-print  'The First Valuse is %s\nThe Second Valuse is %s' %(i,j)
-Mod2(i)
+print my_bool, my_float, my_int
 
+division=108/9
+print division
 
+a=12
+print hex(a)
+print oct(a)
 
-## test ##
+a = "Life is too short, You need Python, Jaehan's bag"
+#a = ' "Jaehan\'s works" '
+#a="""
+#Hi My name is Jaehan
+#I have a family
+#Jaehan's bag
+#He said "it's too big"
+#"""
+print a[3]
+b = len(a)
+print(b)
+i=0
+# while i < b+1:
+#    print a[i:b]
+#    # text= "text is" + a[i]
+#    i = i+1
+#    #print (text)
+print (a[0:b])
+
+#print 'i eat %s haha' % a
+#print "%10s" % "hi"
+print a.count("e")
+
+### TEST ###
